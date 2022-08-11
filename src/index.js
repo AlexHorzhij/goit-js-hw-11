69537d79e748d618efc5b2967aa894e202f4d952
OleksandrHorzhij <horzhij@gmail.com>
2022-08-11 22:27:38 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import NewApiForFetch from './js/js-api';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', });
-
 const newFetchApi = new NewApiForFetch()
 
 const refs = {
@@ -15,47 +13,94 @@ const refs = {
     subBtn: document.querySelector('.search-btn'),
     gallery: document.querySelector('.gallery'),
     loadBtn: document.querySelector('.load-more'),
-    btnDiv: document.querySelector('.btn-div')
+    btnDiv: document.querySelector('.btn-div'),
+    body: document.querySelector('body'),
+    info: document.querySelector('.load-info')
 }
 
 refs.form.addEventListener('submit', onSubmit)
-refs.loadBtn.addEventListener('click', loadMore)
+// refs.loadBtn.addEventListener('click', loadMore)
+window.addEventListener('scroll', autoLoadContent)
 
-function loadMore() {
+
+// LOAD WITH BUTTON
+// function loadMore() {
+//     newFetchApi.goFetch().then((data) => {
+//         const respArr = data.data.hits
+//         markupCards(respArr)
+//         scrollGallery()
+
+//         const pageCount = Math.ceil(data.data.totalHits / 40)
+//         const currentPage = newFetchApi.page
+
+//         if (pageCount < currentPage) {
+//             refs.loadBtn.classList.add('is-hidden')
+//             Notify.info("We're sorry, but you've reached the end of search results.")
+//         }
+//     }).catch(error => console.log("error"))
+// }
+
+// function scrollGallery() {
+//     const { height: cardHeight } = refs.gallery.firstElementChild.getBoundingClientRect()
+
+//     window.scrollBy({
+//         top: cardHeight * 2,
+//         behavior: "smooth",
+//     });
+// }
+
+// function onSubmit(e) {
+//     e.preventDefault()
+//     refs.gallery.innerHTML = ""
+//     refs.loadBtn.classList.add('is-hidden')
+//     newFetchApi.resetPage()
+
+//     newFetchApi.query = refs.input.value.trim()
+//     newFetchApi.goFetch().then((data) => {
+//         Notify.success(`Hooray! We found ${data.data.totalHits} images.`);
+
+//         const respArr = data.data.hits
+//         const pageCount = Math.ceil(data.data.totalHits / 40)
+//         const currentPage = newFetchApi.page
+
+//         if (pageCount >= currentPage) {
+//             refs.loadBtn.classList.remove('is-hidden')
+//         }
+//         markupCards(respArr)
+//     }).catch(error => console.log(error))
+// }
+
+function autoLoadContent() {
+    const rect = document.documentElement.getBoundingClientRect()
+    if (rect.bottom < document.documentElement.clientHeight+150) {
+        loadMoreInfinity()
+    }
+}
+
+function loadMoreInfinity() {
     newFetchApi.goFetch().then((data) => {
         const respArr = data.data.hits
-        markupCards(respArr)
-        console.log(newFetchApi.page)
-
-        const pageCount = Math.ceil(data.data.totalHits / 40)
-        const currentPage = newFetchApi.page
-
-        if (pageCount < currentPage) {
-            refs.loadBtn.classList.add('is-hidden')
-            Notify.info("We're sorry, but you've reached the end of search results.")
-        }
-    }).catch(error => console.log("error"))
+        markupCards(respArr)  
+        
+    }).catch(error => {
+        console.log(error)
+    })
+    refs.info.classList.remove('is-hidden')
 }
 
 function onSubmit(e) {
     e.preventDefault()
     refs.gallery.innerHTML = ""
-    refs.loadBtn.classList.add('is-hidden')
     newFetchApi.resetPage()
+    refs.info.classList.add('is-hidden')
 
     newFetchApi.query = refs.input.value.trim()
     newFetchApi.goFetch().then((data) => {
-        Notify.success(`Hooray! We found ${data.data.totalHits} images.`);
-
         const respArr = data.data.hits
-        const pageCount = Math.ceil(data.data.totalHits / 40)
-        const currentPage = newFetchApi.page
-
-        if (pageCount >= currentPage) {
-            refs.loadBtn.classList.remove('is-hidden')
-        }
-
         markupCards(respArr)
+    if (data.data.total !== 0) {
+    Notify.success(`Hooray! We found ${data.data.totalHits} images.`);
+    }
     }).catch(error => console.log(error))
 }
 
@@ -84,26 +129,18 @@ function markupCards(dataArr) {
     </a>`
 }, "")
     refs.gallery.insertAdjacentHTML("beforeend", markup)
+    const lightbox = new SimpleLightbox('.gallery a');
+    lightbox.refresh()
 }
-
 
 Notify.init({
     width: '700px',
     fontSize: '30px',
     borderRadius: '10px',
-    position: 'center-top',
+    // position: 'right-top',
+    position: 'right-top',
     fontAwesomeIconSize: '64px',
 });
 
 
-// function hideLoadBtn() {
-//     const pageCount = data.data.totalHits / 40
-//     const currentPage = newFetchApi.page
 
-//             if ( pageCount < currentPage) {
-            
-//         }
-
-//     refs.loadBtn.classList.add('is-hidden')
-//     Notify.failure("We're sorry, but you've reached the end of search results.")
-// }
